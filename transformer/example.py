ad91b3f9670788c4ba2cedb0fdb10b01bb4ce0ca
Yanbo Liang <ybliang8@gmail.com>
2018-09-19 14:58:27 -0700

    Update transformer example

--- transformer/example.py
+++ transformer/example.py
@@ -1,7 +1,5 @@
 import numpy as np
 
-from keras.utils import to_categorical
-
 from transformer import Transformer
 from test_utils import printoptions
 from model_params import TransformerBaseParams
@@ -13,35 +11,32 @@ if __name__ == '__main__':
     params = TransformerBaseParams()
 
     num_examples = 100
-    seq_len_x = 10
-    seq_len_y = 10
+    x_len = 10
+    y_len = 10
 
     def set_input_padding(raw):
-        nsize = len(raw)
-        rlen = len(raw[0])
-        assert rlen > 3
-        for i in range(nsize):
-            tlen = np.random.randint(3, rlen + 1)
-            raw[i][tlen - 1] = 1
-            for j in range(tlen, rlen):
+        num_examples = len(raw)
+        num_timesteps = len(raw[0])
+        assert num_timesteps > 3
+        for i in range(num_examples):
+            num_non_padding = np.random.randint(3, num_timesteps + 1)
+            raw[i][num_non_padding - 1] = 1
+            for j in range(num_non_padding, num_timesteps):
                 raw[i][j] = 0
 
-    my_input_x_raw = np.random.randint(2, params.vocab_size, size=(num_examples, seq_len_x))
-    my_input_y_raw = np.random.randint(2, params.vocab_size, size=(num_examples, seq_len_y))
-    set_input_padding(my_input_x_raw)
-    set_input_padding(my_input_y_raw)
+    x = np.random.randint(2, params.vocab_size, size=(num_examples, x_len))
+    y = np.random.randint(2, params.vocab_size, size=(num_examples, y_len))
+
+    set_input_padding(x)
+    set_input_padding(y)
 
     transformer = Transformer(params)
 
-    transformer.compile(optimizer="Adam",
-                  loss=transformer.get_loss())
+    transformer.compile(optimizer="Adam", loss=transformer.get_loss())
 
-    transformer.fit(x=[my_input_x_raw, my_input_y_raw],
-                    y=my_input_y_raw,
-                    epochs=1,
-                    batch_size=256)
+    transformer.fit(x=[x, y], y=y, epochs=1, batch_size=256)
 
-    pred = transformer.predict(x=[my_input_x_raw[0:10], None], batch_size=256)
+    pred = transformer.predict(x=[x[0:10], None], batch_size=256)
 
     print("pred result (first 100):")
     with printoptions(threshold=3000):
